ac34c7407ec4d8d81f331c7d1d9cd552abeff0d9
Kingsley <kingsleykoech@gmail.com>
2026-03-14 03:02:58 +0200

    Task 2: Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	char c;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
