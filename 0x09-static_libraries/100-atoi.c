e104f570027a50f699fe4c0483ba680ff40cb469
kingsleykipkoech <kingsleykoech@gmail.com>
2026-04-11 09:43:35 +0200

    0x09-static_libraries: Add isdigit, memcpy, strncat, atoi

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+	int i, sign, result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
