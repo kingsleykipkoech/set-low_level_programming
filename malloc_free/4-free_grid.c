0ed9494ae1953aa5f0a1856720b5e4bac5418f52
Kingsley Kipkoech <kingsleykoech@gmail.com>
2026-05-18 05:01:55 +0200

    Add free_grid to properly deallocate 2D grid memory

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees a 2D grid previously created by alloc_grid
+ * @grid: the 2D grid to free
+ * @height: the height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
