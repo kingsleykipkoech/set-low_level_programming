ec54ba8f6180dfa9ba8d8c0e607161113fb7a824
Kingsley Kipkoech <kingsleykoech@gmail.com>
2026-05-23 03:10:23 +0200

    Add lists.h and README.md

---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# More Singly Linked Lists
+
+Project for learning more singly linked list operations.
---
+++ more_singly_linked_lists/lists.h
@@ -0,0 +1,37 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int _putchar(char c);
+int sum_listint(listint_t *head);
+
+#endif /* LISTS_H */
