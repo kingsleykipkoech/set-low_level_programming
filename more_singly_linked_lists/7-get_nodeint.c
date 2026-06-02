be9d113c3ced0c11b1b54ab6c96770ff12e613b9
Kingsley Kipkoech <k.kipkoech@alustudent.com>
2026-06-02 16:57:41 +0200

    Implement tasks 0-7 of singly linked lists project

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node, starting at 0
+ *
+ * Return: pointer to the nth node, or NULL if it doesn't exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+
+	return (NULL);
+}
